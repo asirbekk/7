b4880a70b40a5cc0d2613ebf39317c616d3f1dfe
asirbekk <78365807+asirbekk@users.noreply.github.com>
2021-03-18 18:00:18 +0300

    Update 7-laba.py

--- 7-laba.py
+++ 7-laba.py
@@ -1,36 +1,38 @@
+  
 #1-заданий
-# text=("C:/05.txt")
-# n= open(text)
+# text=("demofile.txt")
+# n=open(text)
 # for line in n:
 #     line.split('.')
 # koma=","
 # for i in line:
-#     if not koma in i:
-#        print (i)
+# 	if not koma in i:
+# 		print (i)
+# f = open("demofile.txt", "r")
+# print(f.read())
 
 #2-заданий
 #
-import json
+# import json
 
-data = {}
-data['people'] = []
-data['people'].append({
-    'товар': 'add',
-    'магазин': 'add',
-    'стой': 'Uzbekiston'
-})
+# data = {}
+# data['people'] = []
+# data['people'].append({
+#     'товар': 'add',
+#     'магазин': 'add',
+#     'стой': 'Uzbekiston'
+# })
+# print(data)
 
 
+# with open('data.txt', 'w') as outfile:
+#     json.dump(data, outfile)
+# import json
 
-with open('data.txt', 'w') as outfile:
-    json.dump(data, outfile)
-import json
-
-with open('data.txt') as json_file:
-    data = json.load(json_file)
-    for p in data['people']:
-        print('Товар: ' + p['товар'])
-        print('Магазин: ' + p['магазин'])
-        print('Стой: ' + p['стой'])
-        print('')
-        
+# with open('data.txt') as json_file:
+#     data = json.load(json_file)
+#     for p in data['people']:
+#         print('Товар: ' + p['товар'])
+#         print('Магазин: ' + p['магазин'])
+#         print('Стой: ' + p['стой'])
+#         print('')
