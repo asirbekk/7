abe4bcec0eb9a417ddaddf1ea996a2ab2864f948
Muhammadqodir <78365807+asirbekk@users.noreply.github.com>
2021-03-23 00:01:34 +0500

    Lab7

--- Zadaniye2.py
+++ Zadaniye2.py
@@ -12,3 +12,14 @@ print(data)
 
 with open('data.txt', 'w') as outfile:
     json.dump(data, outfile)
+
+import json
+
+with open('data.txt') as json_file:
+    data = json.load(json_file)
+    for p in data['people']:
+        print('Товар: ' + p['товар'])
+        print('Магазин: ' + p['магазин'])
+        print('Стой: ' + p['стой'])
+        print('')
+
